15e4274c5e8ff8a51f3fd1bf672ef97e7e5da10f
RohanMukka <rohanmukka09@gmail.com>
2026-08-30 13:24:11 -0500

    fix: remove depth of field to ensure car stays crisp during all camera moves

--- src/scene/Effects.tsx
+++ src/scene/Effects.tsx
@@ -1,4 +1,4 @@
-import { EffectComposer, Bloom, ChromaticAberration, Vignette, Noise, SMAA, DepthOfField } from '@react-three/postprocessing'
+import { EffectComposer, Bloom, ChromaticAberration, Vignette, Noise, SMAA } from '@react-three/postprocessing'
 import { BlendFunction } from 'postprocessing'
 import * as THREE from 'three'
 import { useMemo } from 'react'
@@ -7,8 +7,7 @@ export function Effects() {
   const ca = useMemo(() => new THREE.Vector2(0.0006, 0.0004), [])
   return (
     <EffectComposer multisampling={0} enableNormalPass={false}>
-      <DepthOfField target={[0, 0.68, 0]} focalLength={0.04} bokehScale={3.0} height={480} />
-      <Bloom intensity={0.62} luminanceThreshold={0.8} luminanceSmoothing={0.3} mipmapBlur radius={0.68} />
+      <Bloom intensity={0.8} luminanceThreshold={0.7} luminanceSmoothing={0.4} mipmapBlur radius={0.8} />
       <ChromaticAberration offset={ca} radialModulation modulationOffset={0.42} blendFunction={BlendFunction.NORMAL} />
       <Vignette offset={0.28} darkness={0.85} />
       <Noise premultiply opacity={0.035} blendFunction={BlendFunction.OVERLAY} />
